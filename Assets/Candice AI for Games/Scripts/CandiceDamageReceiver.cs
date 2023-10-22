d65030594571fad29a000ee45cb6e4636dcf517e
artem.khrulkov <artemhrulkov@gmail.com>
2023-10-22 20:48:47 +0500

    Added die statement

--- Assets/Candice AI for Games/Scripts/CandiceDamageReceiver.cs
+++ Assets/Candice AI for Games/Scripts/CandiceDamageReceiver.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
+﻿using CandiceAIforGames.AI;
+using UnityEngine.SceneManagement;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CandiceDamageReceiver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    public float hitPoints = 3f;
+
+    [SerializeField]
+    public bool isAttacking = true;
+    public float HitPoints { get => hitPoints; set => hitPoints = value; }
+    public CandiceModuleCombat combatModule;
+    public bool IsAttacking { get => isAttacking; set => isAttacking = value; }
 
     // Update is called once per frame
-    void Update()
+    void Start() 
     {
-        
+        combatModule = new CandiceModuleCombat(transform, onAttackComplete);
+    }
+    public void CandiceReceiveDamage(float damage)
+    {
+        HitPoints = combatModule.ReceiveDamage(damage, HitPoints);
+        //Animations Manager
+        //End
+        if(HitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //if kill cam is attached
+        Debug.Log("Die!");
+        Destroy(this.gameObject, 0.5f);
+    }
+
+    void onAttackComplete(bool success)
+    {
+        IsAttacking = false;
     }
 }
